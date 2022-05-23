66afeed6e475140b6cf084f39a5b234d6fd4176e
jhonv4sq <jhonv4sq.01@gmail.com>
2022-05-23 14:43:55 +0200

    update 23/05/2022

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
+                <div class="card-header">{{ __('Login') }} <i class="fa-solid fa-user"></i></div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
@@ -68,6 +68,9 @@
                 </div>
             </div>
         </div>
+
+        <example-component />
+        
     </div>
 </div>
 @endsection
